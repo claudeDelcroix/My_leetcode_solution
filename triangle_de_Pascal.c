ee93abdb9864c3fc3cc9ba10805558c8ebbf7178
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-09-14 23:37:34 +0300

    Create triangle_de_Pascal.c

---
+++ triangle_de_Pascal.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void afficherTrianglePascal(const int n) {
+    int triangle[n][n];
+
+    for (int ligne = 0; ligne < n; ligne++) {
+        for (int i = 0; i <= ligne; i++) {
+            if (i == 0 || i == ligne) {
+                triangle[ligne][i] = 1;
+            } else {
+                triangle[ligne][i] = triangle[ligne-1][i-1] + triangle[ligne-1][i];
+            }
+            printf("%d ", triangle[ligne][i]);
+        }
+        printf("\n");
+    }
+}
+
+int** generate(const int numRows, int* returnSize, int** returnColumnSizes) {
+    *returnSize = numRows;
+    int** pascal = (int**)malloc(numRows*sizeof(int*));
+    (*returnColumnSizes) = (int*)malloc(numRows*sizeof(int));
+
+    for(int i = 0;i<numRows;i++){
+        (*returnColumnSizes)[i] = i+1;
+        pascal[i] = (int*)malloc((i+1)*sizeof(int));
+    }
+    if(numRows == 1){
+        pascal[0][0] = 1;
+        return pascal;
+    }
+    pascal[0][0] = 1;
+    pascal [1][0] = 1;
+    pascal[1][1] = 1;
+
+    for(int i = 2;i<numRows;i++){
+        pascal[i][0] = 1;
+        pascal[i][i] = 1;
+        for(int j = 1;j<i;j++){
+            pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+        }
+    }
+
+    return pascal;
+}
+
+
+int main(void) {
+    printf("Bienvenue!, ce programmer permet la construction du triangle de Pascal\n");
+    printf("entrez le nombre de ligne :   ");
+
+    int nbr_ligne = 0,returnSize = 0,*returnColumnSizes;
+    scanf("%d",&nbr_ligne);
+    printf("\n lancement de la construction du triangle de Pascal pour %d lignes\n",nbr_ligne);
+    int** Pascal_triangle = generate(nbr_ligne,&returnSize,&returnColumnSizes);
+
+    printf("Affichage du triangle de pascal...\n");
+    for(int i = 0;i<nbr_ligne;i++) {
+        for(int j = 0;j<returnColumnSizes[i];j++) {
+            printf("%d ",Pascal_triangle[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i = 0;i<nbr_ligne;i++) {
+        free(Pascal_triangle[i]);
+    }
+    free(Pascal_triangle);
+    free(returnColumnSizes);
+    return 0;
+}
