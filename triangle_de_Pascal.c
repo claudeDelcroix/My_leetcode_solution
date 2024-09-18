a9e1ec77501a4406a9dfcb224606b95482b36df2
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-09-18 14:47:28 +0300

    ajout de la fonction getRow pour recuperer une seule ligne du triangle

--- triangle_de_Pascal.c
+++ triangle_de_Pascal.c
@@ -45,6 +45,43 @@ int** generate(const int numRows, int* returnSize, int** returnColumnSizes) {
     return pascal;
 }
 
+//nouvelle fonction au nom de triangle de Pascal 2
+//cette fonction permet de retourner un tableau a une dimension correspondant a la n-eme ligne du triangle avec cette entier n entrer par l'utilisateur 
+int* getRow(int rowIndex, int* returnSize) {
+    rowIndex+=1;
+    *returnSize = rowIndex;
+    int** pascal = (int**)malloc((rowIndex)*sizeof(int*));
+   int* result = (int*)malloc((rowIndex )*sizeof(int));
+
+      for(int i = 0;i<rowIndex;i++){
+        pascal[i] = (int*)malloc((i+1)*sizeof(int));
+      }
+      if(rowIndex == 1){
+        result[0] = 1;
+        return result;
+      }
+      pascal[0][0] = 1;
+      pascal [1][0] = 1;
+      pascal[1][1] = 1;
+
+      for(int i = 2;i<rowIndex ;i++){
+        pascal[i][0] = 1;
+        pascal[i][i] = 1;
+        for(int j = 1;j<i;j++){
+           pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+        }
+      }
+
+       for(int i = 0;i<rowIndex;i++){
+        result[i] = pascal[rowIndex-1][i];
+       }
+    for(int i = 0;i<rowIndex;i++){
+          free(pascal[i]);
+    }
+    free(pascal);
+        
+      return result;
+}
 
 int main(void) {
     printf("Bienvenue!, ce programmer permet la construction du triangle de Pascal\n");
