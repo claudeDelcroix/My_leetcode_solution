c9de9f30e66b2e569c071a86bc8187240ecafce4
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-09-20 23:04:30 +0300

    ajout d'une fonction beaucoup plus optimise

--- maxProfit.c
+++ maxProfit.c
@@ -12,3 +12,29 @@ int maxProfit(int* prices, int pricesSize) {
 
     return maxProfit;
 }
+int maxProfit(int* prices, int pricesSize) {
+
+    if (pricesSize < 2) {
+        return 0;
+    }
+
+    int min_price = prices[0];
+    int max_profit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        // Update the minimum price so far
+        if (prices[i] < min_price) {
+            min_price = prices[i];
+        }
+
+        // Calculate the profit if we sold at the current price
+        int profit = prices[i] - min_price;
+
+        // Update the maximum profit so far
+        if (profit > max_profit) {
+            max_profit = profit;
+        }
+    }
+
+    return max_profit;
+}
