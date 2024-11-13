157e5bd515e5967cb6cdba34c7ada1c9f2dd9249
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-11-13 22:27:32 +0300

    Update containsDuplicate2.c

--- containsDuplicate2.c
+++ containsDuplicate2.c
@@ -78,3 +78,112 @@ bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
     deleteHashTable(Table);
     return false;
 }
+
+//le code suivant a 0ms
+//****************************************************************************************************************************
+/*bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+    int i, j;
+    for(i = 0; i < numsSize-1; i++){
+        for(j = i+1; j <= i + k && j < numsSize; j++){
+            if(nums[i] == nums[j])
+                return 1;
+        }
+    }
+
+    return 0;
+}*/
+
+/*typedef struct HashNode {
+    int key;
+    int value;
+    struct HashNode* next;
+} HashNode;
+
+typedef struct {
+    HashNode** buckets;
+    int size;
+} HashMap;
+
+HashMap* createHashMap(int size) {
+    HashMap* map = (HashMap*)malloc(sizeof(HashMap));
+    map->buckets = (HashNode**)malloc(size * sizeof(HashNode*));
+    for (int i = 0; i < size; ++i) {
+        map->buckets[i] = NULL;
+    }
+    map->size = size;
+    return map;
+}
+
+int hashFunction(int key, int size) {
+    return abs(key) % size;
+}
+
+void put(HashMap* map, int key, int value) {
+    int hashIndex = hashFunction(key, map->size);
+    HashNode* newNode = (HashNode*)malloc(sizeof(HashNode));
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = map->buckets[hashIndex];
+    map->buckets[hashIndex] = newNode;
+}
+
+bool get(HashMap* map, int key, int* value) {
+    int hashIndex = hashFunction(key, map->size);
+    HashNode* current = map->buckets[hashIndex];
+    while (current != NULL) {
+        if (current->key == key) {
+            *value = current->value;
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+void freeHashMap(HashMap* map) {
+    for (int i = 0; i < map->size; ++i) {
+        HashNode* current = map->buckets[i];
+        while (current != NULL) {
+            HashNode* temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    free(map->buckets);
+    free(map);
+}
+
+bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+    HashMap* map = createHashMap(numsSize);
+    
+    for (int i = 0; i < numsSize; ++i) {
+        int index;
+        if (get(map, nums[i], &index)) {
+            if (i - index <= k) {
+                freeHashMap(map);
+                return true;
+            }
+        }
+        put(map, nums[i], i);
+    }
+    freeHashMap(map);
+    return 0;*/
+
+    #define MAX 300000
+    bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+    int record[MAX];
+
+    memset(record, -1, sizeof(record));
+
+    for(int i = 0, tmp; i < numsSize; i++){
+        tmp = ((nums[i] % MAX) + MAX) % MAX;
+
+        if(record[tmp] != -1 && i - record[tmp] <= k){
+            return true;
+        }
+
+        record[tmp] = i;
+    }
+    return false;
+}
+//}
