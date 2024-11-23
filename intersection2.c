afa654ac7ed1bfd0edece0326d87f2ff0154e9d4
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-11-24 01:06:23 +0300

    Update intersection2.c

--- intersection2.c
+++ intersection2.c
@@ -88,3 +88,60 @@ int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* return
     deleteHashTable(table);
     return array;
 }
+
+//moyen de resoudre le probleme avec Qsort fonction pour trier de facon generique
+//et rearangement de la menoire a la fin
+int compare(const void* a, const void* b){
+    return *(int*)a - *(int*)b;
+}
+
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int)*1001);
+
+    qsort(nums1, nums1Size, sizeof(int), compare);
+    qsort(nums2, nums2Size, sizeof(int), compare);
+
+    int length = 0, i, j;
+
+    for(i = 0, j = 0; i < nums1Size && j < nums2Size; i++, j++){
+        if(nums1[i] == nums2[j])
+            result[length++] = nums1[i];
+        else if(nums1[i] < nums2[j])
+            j--;
+        else
+            i--;
+    }
+
+    *returnSize = length;
+    result = realloc(result, sizeof(int)*length);
+    return result;
+}
+
+//avec le moins de memoire sur l'exercice
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+
+    int* t = (int*)malloc(sizeof(int) * (nums1Size > nums2Size ? nums1Size : nums2Size));
+    int i = 0, j = 0, k = 0;
+    *returnSize = 0;
+
+
+    for(i = 0; i < nums2Size; i++) {
+
+        for(j = 0; j < nums1Size; j++) {
+
+            if(nums2[i] == nums1[j]) {
+
+                t[k] = nums2[i];
+                (*returnSize)++;
+                k++;
+                nums1[j] = -1;
+                break;
+            }
+        }
+    }
+
+    return t;
+}
